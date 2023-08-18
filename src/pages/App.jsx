393a13fe02aa2b52a153d0ab76e7429795ee7cbe
dudsssenai <eduardagomesdesouza8@gmail.com>
2023-08-18 09:28:56 -0300

    Criado componente movieCard Adaptações no App.jsx Criado arquivo movieCard.module.css falta código no arquivo movieCard.module.css

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,12 +1,16 @@
 import { useEffect } from "react";
 import { useState } from "react";
+
 import logo from "../assets/devflix.png";
 import searchIcon from "../assets/search.svg";
 
 import "./App.css";
+import MovieCard from "../components/movieCard/movieCard";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
   const apiKey = "e4d577fa";
   const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
 
@@ -19,6 +23,7 @@ const App = () => {
     const data = await response.json();
 
     console.log(data);
+    setMovies(data.Search);
   };
 
   const handleKeyPress = (e) => {
@@ -35,7 +40,6 @@ const App = () => {
         <input
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          onChange={(e) => setSearchTerm(e.target.value)}
           onKeyDown={handleKeyPress}
           placeholder="Pesquise por filmes"
         />
@@ -45,6 +49,15 @@ const App = () => {
           onClick={() => searchMovies(searchTerm)}
         />
       </div>
+    {movies?.length > 0 ? (
+      <div className="container">
+        {movies.map((movie) => (<MovieCard key={movie.imdbID} movies ={movie}/>))}
+      </div>
+    ) : (
+      <div className="empty">
+        <h2>Nenhum filme encontrado 🥺</h2>
+      </div>
+    )}
     </div>
   );
 };
